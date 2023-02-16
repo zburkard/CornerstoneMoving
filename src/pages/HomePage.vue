d0ac2e0ecc9a0f6e24b5cc2a49e0200ac49552ee
Zak Burkard <zbtoyota@gmail.com>
2023-02-16 13:16:47 -0700

    commit

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,6 +1,10 @@
 <template>
-  <section class="container-fluid">
-
+  <section class="container-fluid mt-2">
+    <div class="row justify-content-center mt-5">
+      <div class="col-10 hero rounded d-flex justify-content-end align-items-end">
+        <button class="btn btn-success quote m-3">Get a Free Quote!</button>
+      </div>
+    </div>
   </section>
 </template>
 
@@ -13,5 +17,13 @@ export default {
 </script>
 
 <style scoped lang="scss">
-
+.hero{
+  background-image: url(../assets/img/hero.png);
+  height: 60vh;
+  width: 80%;
+}
+.quote{
+  height: 7vh;
+  width: 30vh;
+}
 </style>
