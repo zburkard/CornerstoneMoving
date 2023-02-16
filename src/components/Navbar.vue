1a1a83356ea4bb276222c9c3b9afbe435d79390b
Zak Burkard <zbtoyota@gmail.com>
2023-02-16 13:05:51 -0700

    commit

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,8 +1,8 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark px-3">
+  <nav class="navbar navbar-expand-lg navbar-dark light px-3">
     <router-link class="navbar-brand d-flex" :to="{ name: 'Home' }">
       <div class="d-flex flex-column align-items-center">
-        <img alt="logo" src="../assets/img/cw-logo.png" height="45" />
+        <img alt="logo" class="p-1 mt-2" src="../assets/img/cornerstone_logo.png" height="90" />
       </div>
     </router-link>
     <button
@@ -16,12 +16,19 @@
     >
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarText">
+    <div class="collapse navbar-collapse d-flex justify-content-between" id="navbarText">
       <ul class="navbar-nav me-auto">
         <li>
-          <router-link :to="{ name: 'About' }" class="btn text-success lighten-30 selectable text-uppercase">
-            About
-          </router-link>
+          <a href="#About" class="cstone rounded text-light selectable p-2 px-4 mx-4">About Us</a>
+        </li>
+        <li>
+          <a href="#About" class="cstone rounded text-light selectable p-2 px-4 mx-4">Our Services</a>
+        </li>
+        <li>
+          <a href="#About" class="cstone rounded text-light selectable p-2 px-4 mx-4">Check Our Schedule</a>
+        </li>
+        <li>
+          <a href="#About" class="cstone rounded text-light selectable p-2 px-4 mx-4">Contact Us</a>
         </li>
       </ul>
       <!-- LOGIN COMPONENT HERE -->
@@ -59,6 +66,9 @@ a:hover {
   nav {
     height: 64px;
   }
+}
+.cstone{
+  background-color: #00538E;
 }
 
 </style>
